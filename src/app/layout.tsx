0f31cfe6915c5b395823ba49269a1642c0fc58d5
IuriMarques9 <iurimarcianom@hotmail.com>
2026-03-26 15:18:29 +0000

    feat: add @vercel/analytics for enhanced tracking in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Providers from "@/app/providers";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,6 +23,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="pt" className={`${geistSans.variable} ${geistMono.variable}`}>
       <body>
+        <Analytics/>
         <Providers>{children}</Providers>
       </body>
     </html>
